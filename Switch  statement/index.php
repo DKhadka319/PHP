60aea0db139b7271a65aa788cbdef68695a27679
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2022-03-16 22:15:46 +1100

    Create index.php

---
+++ Switch  statement/index.php
@@ -0,0 +1,30 @@
+
+<?php
+$level= 44;
+ 
+switch ($level){
+    case 1:
+        echo "You are at the level 3 ";
+        break;
+        case 2:
+            echo "You are at the level 6 ";
+            break;
+            case 3:
+            echo "You are at the level 7 ";
+            break;
+            case 4:
+            echo "You are at the level 9";
+            break;
+            case 5:
+            echo "You are at the level 0 ";
+            break;
+            case 6:
+            echo "You are at the level 90 ";
+            break;
+            default:
+            echo "the level are not meet";
+ 
+ 
+ 
+}
+?>
