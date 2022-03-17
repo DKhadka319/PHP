6b3c886bbda3f74cf293f59d41f092e7ec6758e5
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2022-03-17 17:30:31 +1100

    Create index.php

---
+++ validation form/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    //here we define all the variable that we are going to use 
+    $fullname=$email=$gender=$comment=$number =$age ="";
+
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $fullname =test_input($_POST["name"]);
+            $email =test_input($_POST['email']);
+            $gender =test_input($_POST['gender']);
+            $comment=test_input($_POST['comment']);
+            $number =test_input($_POST['number']);
+            $age =test_input($_POST['age']);
+        }
+        function test_input($data){
+            $data =trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+    ?>
+    <h1>Making the form</h1>
+
+<h2>Registraction Form 
+
+</h2>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"  method="POST">
+Full Name: <input type="text" name="name">
+<br> <br>
+E-mail: <input type ="email" name="email">
+<br> <br>
+Number (optional): <input type="text" name="number">
+<br> <br>
+Age: <input type="text" name="age">
+<br> <br>
+Comment: <textarea name="comment" rows="10" cols="30"></textarea>
+<br> <br>
+Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+<br> <br>
+<input type="submit" name="click here" value="click here">
+    </form>
+<?php
+echo "<h2> Your Input:</h2>";
+echo $fullname;
+echo "<br>";
+echo $email;
+echo "<br>";
+    
+echo $age;
+echo "<br>";
+    
+echo $number;
+echo "<br>";
+    
+echo $comment;
+echo "<br>";
+    
+echo $gender;
+?>
+    
+</body>
+</html>
